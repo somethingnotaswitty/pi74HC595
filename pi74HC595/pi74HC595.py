a78d8c7bb66a3dac4cb5b66b0f5961ffab747f24
mnchur <mnchur@gmail.com>
2021-03-14 22:58:20 -0400

    adding pwm stuff

--- pi74HC595/pi74HC595.py
+++ pi74HC595/pi74HC595.py
@@ -3,13 +3,13 @@ import RPi.GPIO as gpio
 
 class pi74HC595:
     def __init__(
-        self, DS: int = 11, ST: int = 13, SH: int = 15, daisy_chain: int = 1,
+        self, DS: int = 11, ST: int = 13, SH: int = 15, OE: int = 33, daisy_chain: int = 1, PWM_FRQ: int = 100
     ):
 
-        if not (isinstance(DS, int) or isinstance(ST, int) or isinstance(SH, int)):
+        if not (isinstance(DS, int) or isinstance(ST, int) or isinstance(SH, int) or isinstance(OE, int)):
             raise ValueError("Pins must be int")
-        elif DS < 1 or DS > 40 or ST < 1 or ST > 40 or SH < 1 or SH > 40:
-            raise ValueError("Pins (DS, ST, SH) must be within pin range")
+        elif DS < 1 or DS > 40 or ST < 1 or ST > 40 or SH < 1 or SH > 40 or OE < 1 or OE > 40:
+            raise ValueError("Pins (DS, ST, SH, OE) must be within pin range")
 
         if not isinstance(daisy_chain, int):
             raise ValueError("daisy_chain must be int")
@@ -19,6 +19,8 @@ class pi74HC595:
         self.data = DS  # DS
         self.parallel = ST  # ST_CP
         self.serial = SH  # SH_CP
+        self.output_enable = OE # OE 
+        self.pwm_frequency = PWM_FRQ # Frequency for PWM pin if used 
         self.daisy_chain = daisy_chain  # Number of 74HC595s
         self.current = [0, 0, 0, 0, 0, 0, 0, 0] * self.daisy_chain
         self._setup_board()
@@ -92,6 +94,25 @@ class pi74HC595:
             raise ValueError("Argument must be within pin range")
 
         self.serial = ST
+
+
+    def set_pwm_pin(self, pin: int ):
+        """
+        Set the pin to be used for PWM
+
+        args: int in range of PWM capable pins
+            - default : pin 33 / gpio 13
+            - Raspberry Pi 4 PWM pins: 12 , 32, 33, 35
+            - Rapsbery Pi 3 PWM pins: 
+
+        returns: None
+        """
+        if not isinstance(pin, int):
+            raise ValueError("Argument must be int")
+        elif pin < 1 or pin > 40:
+            raise ValueError("Argument must be within pin range")
+        
+        self.output_enable = OE
 
     def set_daisy_chain(self, num: int):
         """
@@ -181,6 +202,29 @@ class pi74HC595:
 
         """
         return self.current
+
+    def set_pwn_frequency(self, value: int):
+        """
+        Set the frequncy for the PWM pin 
+
+        args: int in range 0-100
+                - pin outputs a square waveform with variable frequency and duty cycle.
+                - This uses the software PWM generator built into the RPi.GPIO lib not hardware PWM.
+                - The minimum PWM output frequency is 10 Hz. The maximum PWM output frequency is 8 KHz.
+                - A duty cycle of 0 means that the waveform is always low. A duty cycle of 1 means the waveform is always high.
+                - Can be any value between 0 (i.e 0%/LOW) and 100 (ie.e 100%/HIGH).
+
+        returns: None
+        """
+        if not isinstance(value, int):
+            raise ValueError("PWM Frequency must be an int from 0 - 100")
+        elif value < 0:
+            raise ValueError("PWM Frequency must be an int from 0 - 100")
+        elif value > 100:
+            raise ValueError("PWM Frequency must be an int from 0 - 100")
+        pi_pwm = gpio.PWM(self.output_enable, value)
+        pi_pwm.start(0)	 
+
 
     def clear(self):
         """
